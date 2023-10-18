2816d491f37f7592572c87f5a7e92d2e5f15e654
BalaM314 <71201189+BalaM314@users.noreply.github.com>
2023-10-18 19:05:02 +0530

    strangeness

--- Youtube-Ad-blocker-Reminder-Remover.user.js
+++ Youtube-Ad-blocker-Reminder-Remover.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Remove Adblock Thing
 // @namespace    http://tampermonkey.net/
-// @version      1.0
+// @version      1.1
 // @description  Removes Adblock Thing
 // @author       JoelMatic, BalaM314
 // @match        https://www.youtube.com/*
